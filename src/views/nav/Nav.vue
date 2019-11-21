4e34229c9d9182642171d66134b24524bda1ded5
xuyingying941024 <1103510569@qq.com>
2019-11-21 21:58:38 +0800

    'second'

---
+++ src/views/nav/Nav.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="nav">
+    <router-link to="/goods" class="nav-item">
+      点餐
+      <i class="line"></i>
+    </router-link>
+    <router-link to="/ratings" class="nav-item">
+      评价
+      <i class="line"></i>
+    </router-link>
+    <router-link to="/seller" class="nav-item">
+      商品
+      <i class="line"></i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appnav"
+  // components:{
+  //   goods,
+  //   ratings,
+  //   seller
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid #e4e4e4;
+}
+.nav-item {
+  flex: 1;
+  color: #666666;
+  text-decoration: none;
+  font-size: 14px;
+  text-align: center;
+  position: relative;
+}
+/* 选中的样式 */
+.active {
+  color: #ffbb22;
+}
+/* 横线样式 */
+.active .line{
+  width: 20px;
+  height: 2px;
+  background-color: #ffbb22;
+  display: inline-block;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  margin-left: -10px;
+}
+</style>

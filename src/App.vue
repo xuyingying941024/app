8282b7dd3ec8d844a7875b3cbfe7084642be2e28
xuyingying941024 <1103510569@qq.com>
2019-11-21 21:50:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <!-- 头部信息 -->
+    <app-header :poiInfoChild = "poiInfo"></app-header>
+    <!-- 导航部分 -->
+    <app-nav/>
+    <!-- 内容 -->
     <router-view/>
   </div>
 </template>
-
+<script>
+import header from "./views/header/Header"
+import nav from "./views/nav/Nav"
+// import header from "./views/header/Header"
+export default {
+  name: "app",
+  components:{
+    "app-header": header,
+    "app-nav": nav
+  },
+  data(){
+    return{
+      poiInfo:{}
+    }
+  },
+  created(){
+    fetch("/api/goods")
+    // 转换数据
+    .then(res => {
+      return res.json()
+    })
+    .then(response => {
+      console.log(response)
+      if(response.data.code == 0){
+        this.poiInfo = response.data.data.poi_info
+      }
+    })
+  }
+}
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
